ef255ac0f57538a2ceac335da112c7c63de39d18
deftclaw <eeccher@cppwind.com>
2021-08-22 16:33:52 -0600

    local updates

---
+++ scrn/default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --mode 1440x900 --pos 1360x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --primary --mode 1360x768 --pos 0x0 --rotate normal --output DP-4 --off --output DP-5 --off --output DP-6 --off --output DP-7 --off
